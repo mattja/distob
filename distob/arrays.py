614929abd9f5100bdfa2760eaa9c87d01bd66f2f
Matthew Aburn <mattja6@gmail.com>
2014-09-24 09:51:32 +1000

    subarray obcaches are view into main obcache

--- distob/arrays.py
+++ distob/arrays.py
@@ -467,6 +467,11 @@ class DistArray(object):
             from distob import engine
             ax = self._distaxis
             self._obcache = concatenate([ra._ob for ra in self._subarrays], ax)
+            # let subarray obcaches and main obcache be views on same memory:
+            for i in range(self._n):
+                ix = [slice(None)] * self.ndim
+                ix[ax] = slice(i, i+1)
+                self._subarrays[i]._obcache = self._obcache[tuple(ix)]
             self._obcache_current = True
             # now prefer local processing:
             self.__engine_affinity__ = (engine.id, self.__engine_affinity__[1])
